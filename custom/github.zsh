f58e9d81964913c59cde02167e167dbceb7388fd
Marcus Wyatt <marcus.wyatt@visfleet.com>
2010-03-12 14:42:53 +1300

    The gh fetch & tracking functions tested and working great.

--- custom/github.zsh
+++ custom/github.zsh
@@ -1,24 +1,22 @@
-function gh_network_remotes_track () {
-  for branch (`git branch -a`); do
-    # git branch --track lowell lowell/master
-    if [[ $branch == remotes/* ]] ; then
-      if [[ $branch != remotes/origin/* ]] ; then
-        tracking_branch=$(echo $branch | sed 's/^.*remotes\/[ ]*//')
-        local_branch_name=`echo $tracking_branch | sed 's/[\/]/_/g'`
-        echo $(git branch --track $local_branch_name $tracking_branch)
-      fi
-    fi
-  done
+# Project network tools for Github
+# REQUIRE: github gem (gem install github)
+
+# Adds all projects in your network as remotes and fetches
+# any objects from them that you don't have yet, and then
+# create local branch and tracks each remote
+function gh_network_fetch_track () {
+  $(github network fetch)
+  gh_network_remotes_track
 }
 
-function gh_network_fetch_track () {
+# Create local branch and tracks each remote
+function gh_network_remotes_track () {
   for branch (`git branch -a`); do
-    # git branch --track lowell lowell/master
     if [[ $branch == remotes/* ]] ; then
       if [[ $branch != remotes/origin/* ]] ; then
         tracking_branch=$(echo $branch | sed 's/^.*remotes\/[ ]*//')
         local_branch_name=`echo $tracking_branch | sed 's/[\/]/_/g'`
-        echo $(git branch --track $local_branch_name $tracking_branch)
+        $(git branch --track $local_branch_name $tracking_branch)
       fi
     fi
   done
